f9ae389f23c7cb9b94e4b943799018fa4c671365
dmxmss <dexammt@gmail.com>
2026-08-24 17:12:07 +0300

    render infinite field

--- renderer/game.js
+++ renderer/game.js
@@ -1,9 +1,10 @@
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 
-const COLS = 10;
-const ROWS = 10;
 const CELL_SIZE = 40;
+
+let cameraX = 0;
+let cameraY = 0;
 
 function resizeCanvas() {
   canvas.width = window.innerWidth;
@@ -16,22 +17,25 @@ function render() {
   ctx.fillStyle = "#222";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-  const fillWidth = COLS*CELL_SIZE;
-  const fillHeight = ROWS*CELL_SIZE;
+  const worldViewportLeft = cameraX - canvas.width/(2*CELL_SIZE);
+  const worldViewportTop = cameraY - canvas.height/(2*CELL_SIZE);
 
-  const offsetX = Math.floor((canvas.width - fillWidth)/2);
-  const offsetY = Math.floor((canvas.height - fillHeight)/2);
+  const startX = Math.floor(worldViewportLeft);
+  const startY = Math.floor(worldViewportTop);
 
-  for (let y = 0; y < ROWS; y++) {
-    for (let x = 0; x < COLS; x++) {
-      drawCell(x, y, offsetX, offsetY);
+  const endX = Math.ceil(worldViewportLeft + canvas.width/CELL_SIZE)
+  const endY = Math.ceil(worldViewportTop + canvas.height/CELL_SIZE)
+
+  for (let y = startY; y < endY; y++) {
+    for (let x = startX; x < endX; x++) {
+      drawCell(x, y, worldViewportLeft, worldViewportTop);
     }
   }
 }
 
-function drawCell(x, y, offsetX, offsetY) {
-  const px = offsetX + x * CELL_SIZE;
-  const py = offsetY + y * CELL_SIZE;
+function drawCell(x, y, worldLeft, worldTop) {
+  const px = CELL_SIZE*(x - worldLeft);
+  const py = CELL_SIZE*(y - worldTop);
 
   ctx.fillStyle = "#aaa";
   ctx.fillRect(
@@ -51,5 +55,35 @@ function drawCell(x, y, offsetX, offsetY) {
 }
 
 window.addEventListener("resize", resizeCanvas);
+
+let dragging = false;
+let lastMouseX = 0;
+let lastMouseY = 0;
+
+canvas.addEventListener("mousedown", (event) => {
+  dragging = true;
+
+  lastMouseX = event.clientX;
+  lastMouseY = event.clientY;
+});
+
+canvas.addEventListener("mousemove", (event) => {
+  if (!dragging) return;
+
+  const dx = event.clientX - lastMouseX;
+  const dy = event.clientY - lastMouseY;
+
+  cameraX -= dx/CELL_SIZE;
+  cameraY -= dy/CELL_SIZE;
+
+  lastMouseX = event.clientX;
+  lastMouseY = event.clientY;
+
+  render();
+});
+
+window.addEventListener("mouseup", () => {
+  dragging = false;
+})
 
 resizeCanvas();
