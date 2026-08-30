34b7d25cd2c31e4dabef05d0aacc996ebaf46fbf
dmxmss <dexammt@gmail.com>
2026-08-30 10:10:12 +0300

    screenToCell function

--- js/game/Camera.js
+++ js/game/Camera.js
@@ -1,15 +1,42 @@
 export class Camera {
-  constructor(worldX, worldY, zoom, cellSize) {
+  constructor(worldX, worldY, zoom, cellSize, viewportWidth, viewportHeight) {
     this.worldX = worldX;
     this.worldY = worldY;
 
     this.zoom = zoom;
     this.cellSize = cellSize;
+
+    this.viewportWidth = viewportWidth;
+    this.viewportHeight = viewportHeight;
   }
 
   // handle camera move (dx, dy in px)
   move(dx, dy) {
     this.worldX -= dx/(CELL_SIZE*this.zoom);  
     this.worldY -= dy/(CELL_SIZE*this.zoom);  
+  }
+
+  // get cell world coordinates from screen coordinates
+  screenToCell(screenX, screenY) {
+    // viewport left-top corner coordinates
+    const worldViewportX = this.worldX - this.viewportWidth/(2*this.cellSize);
+    const worldViewportY = this.worldY - this.viewportHeight/(2*this.cellSize);
+
+    // world coordinates relative to viewport left-top corner
+    const worldX = x/this.cellSize;
+    const worldY = -y/this.cellSize;
+
+    // position relative to camera
+    const localX = worldViewportX + worldX;
+    const localY = worldViewportY + worldY;
+
+    // world position
+    const globalX = localX + this.worldX;
+    const globalY = localY + this.worldY;
+
+    return {
+      x: Math.floor(globalX),
+      y: Math.floor(globalY),
+    }
   }
 }
--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 import { Camera } from "./game/Camera.js";
+import { InputManager } from "./input/InputManager.js";
 
 const CELL_SIZE = 40;
+const canvas = document.getElementById("game");
+const ctx = canvas.getContext("2d");
 
 const go = new Go();
 
@@ -12,3 +15,5 @@ const result = await WebAssembly.instantiateStreaming(
 go.run(result.instance);
 
 camera = new Camera(0, 0, 1, CELL_SIZE);
+
+inputManager = new InputManager(canvas, camera.screenToCell);
